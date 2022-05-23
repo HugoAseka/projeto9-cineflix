3fe2a4c799f0d1771219f8e84ddee091fe80cee4
HugoAseka <hugomaseka@gmail.com>
2022-05-23 11:07:47 -0300

    terminado styled components

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function MovieList() {
   const [movieListArr, setMovieListArr] = useState([]);
@@ -12,18 +13,43 @@ export default function MovieList() {
       setMovieListArr([...response.data]);
     });
   }, []);
-  
 
   return (
-    <div className="homepage">
+    <Homepage>
       <span>Selecione o filme</span>
-      <div className="movie-list">
+      <List>
         {movieListArr.map((obj, index) => {
           return (
-            <Link key={index} id={obj.id} to={`/filme/${obj.id}`}  ><img   src={obj.posterURL} alt="filme" /></Link>
+            <Link key={index} id={obj.id} to={`/filme/${obj.id}`}>
+              <img src={obj.posterURL} alt="filme" />
+            </Link>
           );
         })}
-      </div>
-    </div>
+      </List>
+    </Homepage>
   );
 }
+
+const Homepage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  span {
+    margin: 40px;
+    font-size: 30px;
+  }
+`;
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 30px;
+  max-width: 612px;
+
+  img {
+    height: 200px;
+  }
+`;
